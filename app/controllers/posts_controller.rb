75de350f8e71763b9fc3e27212dbaeae9cf71d65
charliechu <charliechu@techbang.com.tw>
2011-06-14 19:19:01 +0800

    CURD

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,47 @@
+class PostsController < ApplicationController
+	# First to show
+	def index
+		# Catch all the data
+		@posts = Post.all
+	end
+
+	# New a empty Post object
+	def new
+		@post = Post.new
+	end
+	
+	# sending by new.html.erb, initial a new post object and save it, then redirect to indext page 
+	def create
+		@post = Post.new(params[:post])
+		@post.save
+
+		redirect_to :action => :index
+	end
+
+	# By using find method to find the data
+	def show
+		@post = Post.find(params[:id])
+	end
+
+	# 1. Find the data 2. Update to database
+	def edit
+		@post = Post.find(params[:id])
+	end
+
+	def update
+		@post = Post.find(params[:id])
+		@post.update_attributes(params[:post])
+
+		redirect_to :action => :show, :id => @post
+	end
+
+
+	# delete the data  you choice 
+	def destroy
+		@post = Post.find(params[:id])
+		@post.destroy
+
+		redirect_to :action => :index
+	end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rails3CRUD::Application.config.secret_token = '01ec3abc10e31edafe6523b51f73dffe896a301a7007eb0aaefedc1618cd61b2aed26bd7b0a30a0c406b54d08e4c172654c416e53ea4d8f523750e51a2a56156'

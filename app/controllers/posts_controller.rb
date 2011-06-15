4171707fe30f31ea1403a316cd0c76dc8ef0b65a
charliechu <charliechu@techbang.com.tw>
2011-06-15 19:00:59 +0800

    Indent, change  redirect_to to render

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,49 +3,46 @@ class PostsController < ApplicationController
   def index
     # Catch all the data
     @posts = Post.all
-	end
+  end
 
-	# New a empty Post object
+  # New a empty Post object
   def new
     @post = Post.new
-	end
-	
-	# sending by new.html.erb, initial a new post object and save it, then redirect to indext page 
-	def create
-	  @post = Post.new(params[:post])
+  end
+  
+  # sending by new.html.erb, initial a new post object and save it, then redirect to indext page 
+  def create
+    @post = Post.new(params[:post])
     if @post.save
       redirect_to post_path(@post)
     else
-      redirect_to new_post_path
+      render :action => new
     end
   end
-
+  
   # By using find method to find the data
   def show
     @post = Post.find(params[:id])
   end
-
+  
   # 1. Find the data 2. Update to database
   def edit
     @post = Post.find(params[:id])
   end
-
   def update
     @post = Post.find(params[:id])
 		
-    if @post.update_attributes(params[:post])
-      redirect_to post_path(@post)
-    else
-      redirect_to edit_post_path(@post) 
-    end
+		if @post.update_attributes(params[:post])
+		  redirect_to post_path(@post)
+		else
+		  render :action => "edit" 
+		end
   end
-
-
+  
   # delete the data  you choice 
   def destroy
     @post = Post.find(params[:id])
     @post.destroy
-
     redirect_to posts_path
   end
 end
